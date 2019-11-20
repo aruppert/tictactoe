83bcfa8aad8ee24722fe22f457deca2e775cd0db
aruppert <56864263+aruppert@users.noreply.github.com>
2019-11-20 12:01:03 +0100

    add emotion (#2)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import "./App.css";
+import styled from "@emotion/styled";
 import Game from "./components/Game";
+
+const Container = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  padding: 20px;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <h1>TicTacToe</h1>
-
       <Game />
-    </div>
+    </Container>
   );
 }
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,25 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background: lightgrey;
+  border: 1px solid #999;
+  float: left;
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 34px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 34px;
+
+  :focus {
+    outline: none;
+  }
+`;
 
 export default function Square({ value, onClick }) {
-  return (
-    <button className="square" onClick={onClick}>
-      {value}
-    </button>
-  );
+  return <Button onClick={onClick}>{value}</Button>;
 }
